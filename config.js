27bb0d234fc436ab6941d0e4fd5bb8a0957132b4
Nikolas Malík <67058498+GeronimoCZE@users.noreply.github.com>
2020-11-08 13:21:48 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	accountName: 'yourusername',
+	password: 'yourpassword',
+	sharedSecret: 'yoursharedsecret',
+	identitySecret: 'youridentitysecret',
+	apiKey: '760B300AA840B3DB8104B67BC65ADA87'
+}
